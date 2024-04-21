75a4c25e715bbe84daa2871dcbefbc2be3218aee
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2024-04-21 11:46:03 +0530

    Create: about-us route for experimental

--- rootnginx.sh
+++ rootnginx.sh
@@ -3,17 +3,26 @@
 CONFIG_FILE_NAME=
 DOMAIN=$1
 
-mk_nginx_basic_conf() {
+mk_nginx_basic_setup() {
    echo "server {" > $CONFIG_FILE_NAME
    echo "   listen 80;" >> $CONFIG_FILE_NAME
    echo "   server_name $DOMAIN;" >> $CONFIG_FILE_NAME
    echo "" >> $CONFIG_FILE_NAME
    echo "   location / {" >> $CONFIG_FILE_NAME
-   echo "      root /usr/share/nginx;" >> $CONFIG_FILE_NAME
+   echo "      root /usr/share/nginx/$DOMAIN;" >> $CONFIG_FILE_NAME
    echo "      index index.html;" >> $CONFIG_FILE_NAME
    echo "   }" >> $CONFIG_FILE_NAME
    echo "}" >> $CONFIG_FILE_NAME
+
+   # make public html
+   PUBLIC_HTML_FOLDER=./html/$DOMAIN
+   mkdir $PUBLIC_HTML_FOLDER
+   mkdir $PUBLIC_HTML_FOLDER/about-us
+
+   # copy files
+   cp ./nginx/index.html $PUBLIC_HTML_FOLDER
+   cp ./nginx/index.html $PUBLIC_HTML_FOLDER/about-us
 }
 
 CONFIG_FILE_NAME=./config/$DOMAIN.conf
-mk_nginx_basic_conf
+mk_nginx_basic_setup
