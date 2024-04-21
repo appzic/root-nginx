460de7f0a7c7c532b3e14d458e59e2f6b5498a04
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2024-04-21 12:08:58 +0530

    Add: certbot setup

--- rootnginx.sh
+++ rootnginx.sh
@@ -2,6 +2,7 @@
 
 CONFIG_FILE_NAME=
 DOMAIN=$1
+COMPOSE_YML=compose.yml
 
 mk_nginx_basic_setup() {
    echo "server {" > $CONFIG_FILE_NAME
@@ -21,5 +22,23 @@ mk_nginx_basic_setup() {
    cp ./nginx/index.html $PUBLIC_HTML_FOLDER
 }
 
+mk_certbot_docker_compose() {
+   cd ssl
+   echo "services:" > $COMPOSE_YML
+   echo "  certbot:" >> $COMPOSE_YML
+   echo "    image: certbot/certbot:latest" >> $COMPOSE_YML
+   echo "    container_name: certbot" >> $COMPOSE_YML
+   echo "    volumes:" >> $COMPOSE_YML
+   echo "      - ./www:/var/www/certbot" >> $COMPOSE_YML
+   echo "      - ./conf:/etc/letsencrypt" >> $COMPOSE_YML
+   echo "    command: |" >> $COMPOSE_YML
+   echo "      certonly" >> $COMPOSE_YML
+   echo "      --webroot -w /var/www/certbot" >> $COMPOSE_YML
+   echo "      --force-renewal" >> $COMPOSE_YML
+   echo "      --email yasithanadeeshan@gmail.com" >> $COMPOSE_YML
+   echo "      -d $DOMAIN" >> $COMPOSE_YML
+}
+
 CONFIG_FILE_NAME=./config/$DOMAIN.conf
-mk_nginx_basic_setup
+# mk_nginx_basic_setup
+mk_certbot_docker_compose
