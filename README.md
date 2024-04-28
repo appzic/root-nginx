7796dec7542aeedbbe95bf37155da94e31ea34c4
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2024-04-28 11:25:32 +0530

    Add: function for reverse proxy setup

--- README.md
+++ README.md
@@ -13,11 +13,7 @@ Use following command to configure HTTP for the Nginx server:
 ```bash
 sh rootnginx.sh init
 ```
-
-After that, restat the Nginx server using following command:
-```bash
-docker compose restart
-```
+And answer the questions
 
 ## Make HTTPs connection.
 A few minutes later, go the [https://www.whatsmydns.net/](https://www.whatsmydns.net/) website and check wether DNS are successfully propegated or not.
@@ -26,41 +22,15 @@ If DNS successfully propergated, use following command to run certbot and config
 ```bash
 sh rootnginx.sh ssl
 ```
+And answer the questions
 
-After that, restart the Nginx server using following command:
+## Setup Nginx reverse proxy with HTTPs
 ```bash
-docker compose restart
+sh rootnginx.sh rp
 ```
+And answer the questions
 
 ## Update All SSL certificates.
 ```bash
 sh rootnginx.sh update
-```
-
-## Nginx reverse proxy setup for HTTPS
-edit the nginx config files `config/<site_url>.conf`
-
-```conf
-server {
-   listen 80;
-   server_name <site_url>;
-
-   return 301 https://$host$request_uri;
-}
-
-server {
-   listen 443 ssl http2;
-   ssl_certificate /etc/letsencrypt/live/<site_url>/fullchain.pem;
-   ssl_certificate_key /etc/letsencrypt/live/<site_url>/privkey.pem;
-   server_name <site_url>;
-
-   location / {
-      proxy_pass http://internalnet:8080;
-      proxy_set_header Host $http_host;
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header X-Forwarded-Proto $scheme;
-   }
-
-}
 ```
--- rootnginx.sh
+++ rootnginx.sh
@@ -109,12 +109,47 @@ fn_update() {
    docker compose up -d
 }
 
+fn_reverse_proxy() {
+
+   read -p "Enter domain or subdomain: " domain
+   read -p "Enter proxy address: " proxy
+
+   nginx_conf_name=./config/$domain.conf
+
+   echo "server {" > $nginx_conf_name
+   echo "   listen 80;" >> $nginx_conf_name
+   echo "   server_name $domain;" >> $nginx_conf_name
+   echo "" >> $nginx_conf_name
+   echo "   return 301 https://\$host\$request_uri;" >> $nginx_conf_name
+   echo "}" >> $nginx_conf_name
+   echo "" >> $nginx_conf_name
+   echo "server {" >> $nginx_conf_name
+   echo "   listen 443 ssl http2;" >> $nginx_conf_name
+   echo "   ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;" >> $nginx_conf_name
+   echo "   ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;" >> $nginx_conf_name
+   echo "   server_name $domain;" >> $nginx_conf_name
+   echo "" >> $nginx_conf_name
+   echo "   location / {" >> $nginx_conf_name
+   echo "      proxy_pass $proxy" >> $nginx_conf_name
+   echo "      proxy_set_header Host \$http_host;" >> $nginx_conf_name
+   echo "      proxy_set_header X-Real-IP \$remote_addr;" >> $nginx_conf_name
+   echo "      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;" >> $nginx_conf_name
+   echo "      proxy_set_header X-Forwarded-Proto \$scheme;" >> $nginx_conf_name
+   echo "   }" >> $nginx_conf_name
+   echo "" >> $nginx_conf_name
+   echo "}" >> $nginx_conf_name
+
+   docker compose down
+   docker compose up -d
+}
+
 fn_help() {
    echo "Usage:  sh rootnginx.sh [OPTIONS]"
    echo ""
    echo "Options:"
    echo "   init     Initialize HTTP configuration"
    echo "   ssl      Initialize HTTPs configuration"
+   echo "   rp       Configure HTTPs reverse proxy"
    echo "   update   Update SSL certificates"
 }
 
@@ -123,6 +158,8 @@ if [ "$1" = "init" ]; then
    fn_init
 elif [ "$1" = "ssl" ]; then
    fn_ssl
+elif [ "$1" = "rp" ]; then
+   fn_reverse_proxy
 elif [ "$1" = "update" ]; then
    fn_update
 else
